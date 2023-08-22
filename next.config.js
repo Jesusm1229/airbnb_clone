488dd9d0f8d95529e702da86c1f9dd7a7e635fa2
Jesus Eduardo Medina Gallo <48733708+Jesusm1229@users.noreply.github.com>
2023-08-22 14:02:04 -0400

    first commit, adding navbar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: [
+          "ucarecdn.com",
+          "cdn.buymeacoffee.com",
+          "res.cloudinary.com",
+          "imgur.com",
+          "i.imgur.com",
+          "cutt.ly",
+          "activity-graph.herokuapp.com",
+          "i.scdn.co",
+          "images.unsplash.com",
+          "m.media-amazon.com", 
+          "imagekit.io",
+          "ik.imagekit.io",
+        ],
+      },
+      typescript: {
+        ignoreBuildErrors: false,
+      },
+}
+
+module.exports = nextConfig;
